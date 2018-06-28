9360bfc14ec99f13ff424e9f510b5f48be897ba3
Mara <levymaratx@gmail.com>
2018-06-28 06:59:22 -0500

    crystal update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,19 +5,27 @@ var previous = 0;
 
 var resetAndStart = function () {
 
+  //empties the crystals
   $(".crystals").empty();
 
-    randomResult = Math.floor(Math.random() * 69 ) + 30;
+  var images = [
+    'http://static1.squarespace.com/static/5644b453e4b0552eb81185b6/5646246ce4b0a33eccdedd2d/56521e66e4b058e88fca5228/1449169817525/?format=1000w',
+    'http://static1.squarespace.com/static/5644b453e4b0552eb81185b6/5646246ce4b0a33eccdedd2d/56521e66e4b058e88fca5228/1449169817525/?format=1000w' ,
+    'http://static1.squarespace.com/static/5644b453e4b0552eb81185b6/5646246ce4b0a33eccdedd2d/56521e66e4b058e88fca5228/1449169817525/?format=1000w',
+    'http://static1.squarespace.com/static/5644b453e4b0552eb81185b6/5646246ce4b0a33eccdedd2d/56521e66e4b058e88fca5228/1449169817525/?format=1000w'
+ ];
+
+  randomResult = Math.floor(Math.random() * 69 ) + 30;
 
   console.log(randomResult);
-
+  
+  //generates four crystals with random numbers
   $("#result").html('Random Result: ' + randomResult);
-  //four crystals with four random numbers
+  //four crystals with four random numbers and the div for the random number
 
   for(var i = 0; i < 4; i++) {
     
     var random = Math.floor(Math.random() * 11) + 1;
-
 
     console.log(random);
 
@@ -26,13 +34,19 @@ var resetAndStart = function () {
           "class": 'crystal',
           "data-random": random
         });
+        crystal.css({
+          "background-image":"url('" + images[i] + "')",
+          "background-size":"cover"
+        });
           
-        crystal.html(random);
+        // crystal.html(random);
 
     $(".crystals").append(crystal);
 
   }
 }
+
+$("#previous").html(previous);
 
 resetAndStart();
 
@@ -44,10 +58,12 @@ $(document).on('click', ".crystal", function() {
 
   previous += num;
 
+  $("#previous").html(previous);
+
   console.log(previous);
 
   if (previous > randomResult) {
-    lost--;
+    lost++;
 
     $("#lost").html(lost);
 
@@ -63,18 +79,10 @@ $(document).on('click', ".crystal", function() {
 
     previous = 0;
 
+
     resetAndStart();
   }
 
 });
-
-
- 
-// <!-- pseudo code
-// game with 4 crystals and random result
-// every crystal needs random number   between 1 - 12
-// a new random number should generate every time we win or lose
-
-//   -->
 
   
